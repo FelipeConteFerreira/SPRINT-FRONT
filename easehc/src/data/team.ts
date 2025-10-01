b4c8632e9239454fd1b9086c86cc2536eb432044
Felipe Conte <felipeconteferreira@gmail.com>
2025-10-01 17:45:14 -0300

    Finalizado

---
+++ easehc/src/data/team.ts
@@ -0,0 +1,31 @@
+import { Member } from '../types'
+
+export const team: Member[] = [
+  {
+    id: 'altamir-lima',
+    nome: 'Altamir Lima',
+    rm: '562906',
+    turma: '1TDSR',
+    fotoUrl: '/imagens/Altamir.jpeg',
+    linkedin: 'https://www.linkedin.com/in/altamir-lima-b2766b206/',
+    github: 'https://github.com/AltamirLima'
+  },
+  {
+    id: 'felipe-conte',
+    nome: 'Felipe Conte',
+    rm: '562248',
+    turma: '1TDSR',
+    fotoUrl: '/imagens/Felipe.jpeg',
+    linkedin: 'https://www.linkedin.com/in/felipe-conte-ferreira-b85747354/',
+    github: 'https://github.com/FelipeConteFerreira'
+  },
+  {
+    id: 'Jonathan Moreira',
+    nome: 'Jonathan Moreira',
+    rm: '565060',
+    turma: '1TDSR',
+    fotoUrl: '/imagens/Jonathan.jpeg',
+    linkedin: 'https://www.linkedin.com/in/jonathan-moreira-811b03355?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    github: 'https://github.com/Jonathanmoreiragomes'
+  }
+]
